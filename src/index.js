211a3e5ffb6cd18127ca2542a44d43be3b9fb96d
saxon1964 <71132278+saxon1964@users.noreply.github.com>
2022-05-03 18:21:42 +0200

    Minimum react project set

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+// bootstrap js
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+// React stuff
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Core styles
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +17,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
