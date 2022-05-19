d7a04cb4fa0c3a8a5e3d1a9f84daec04e45a7cd5
saxon1964 <71132278+saxon1964@users.noreply.github.com>
2022-05-19 10:44:55 +0200

    Remove globsal vars from utils.js

--- src/misc/utils.js
+++ src/misc/utils.js
@@ -101,12 +101,11 @@ export function decrypt(password, encryptedText) {
   return sjcl.decrypt(password, encryptedText)
 }
 
-const monthNames = {
-  '01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun',
-  '07': 'Jul', '08': 'Avg', '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'
-}
-
 export function formatIsoDate(date) {
+  const monthNames = {
+    '01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun',
+    '07': 'Jul', '08': 'Avg', '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'
+  }
   if(date && date.length == 10 && date[4] == '-' && date[7] == '-') {
     const year = date.substring(0, 4)
     const month = date.substring(5, 7)
