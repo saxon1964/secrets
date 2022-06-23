f3a8bcbecf2fd88e373942d1efb695ed181922f3
samarkov <samarkov@cisco.com>
2022-06-23 12:05:30 +0200

    Removing file upload feature as inadequate solution

--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -3,21 +3,13 @@ import * as Utils from '../misc/utils.js'
 import NameField from './NameField.js'
 import Mandatory from './Mandatory.js'
 
-const NO_FILE = { name: '', size: '', content: '' }
-
 const EditNote = ({id, data, submitData}) => {
   const [name, setName] = React.useState(data.name || '')
   const [note, setNote] = React.useState(data.note || '')
-  const [existingFile, setExistingFile] = React.useState(data.file || NO_FILE)
-  const [selectedFile, setSelectedFile] = React.useState(NO_FILE)
-  const [removeFile, setRemoveFile] = React.useState(false)
 
   React.useEffect(() => {
     setName(data.name || '')
     setNote(data.note || '')
-    setExistingFile(data.file || NO_FILE)
-    setSelectedFile(NO_FILE)
-    setRemoveFile(false)
   }, [data.name])
 
   const checkForm = (e) => {
@@ -33,34 +25,9 @@ const EditNote = ({id, data, submitData}) => {
         type: data.type,
         name: name,
         note: note,
-        file: existingFile
       }
-      if (removeFile && selectedFile.name == '') {
-        newData.file = NO_FILE
-      }
-      else if (selectedFile.name != '') {
-        newData.file = selectedFile
-      }
-      //console.log(newData)
       submitData(id, newData)
     }
-  }
-
-  const handleFileSelect = (e) => {
-    if (e.target.files.length == 0) {
-      setSelectedFile(NO_FILE)
-      return
-    }
-    const file = e.target.files[0]
-    const reader = new FileReader()
-    reader.addEventListener('load', (event) => {
-      setSelectedFile({
-        name: file.name,
-        size: file.size,
-        content: event.target.result
-      })
-    })
-    reader.readAsDataURL(file)
   }
 
   return (
@@ -72,26 +39,9 @@ const EditNote = ({id, data, submitData}) => {
           <div className="col-lg-6 mt-2">
             <NameField id={id} name={name} setName={setName}/>
           </div>
-          <div className="col-lg-6 mt-2" style={{wordWrap: 'anywhere'}}>
-            {existingFile.name == '' && <label htmlFor="fileSelector">Existing file: none</label>}
-            {existingFile.name != '' &&
-              <span>
-                <label htmlFor="fileSelector">Existing file:</label>&nbsp;&nbsp;
-                <a href={existingFile.content} title={`${existingFile.size} bytes`} target="_self" download={existingFile.name}>{existingFile.name}</a>
-                &nbsp;&nbsp;&nbsp;
-                <input className="form-check-input" type="checkbox" id="removeFileCheck" onChange={(e) => setRemoveFile(!removeFile)} checked={removeFile}/>
-                &nbsp;
-                <label className="form-check-label" htmlFor="removeFileCheck">delete</label>
-              </span>
-            }
-            <input type="file" id="fileSelector" className="form-control" onChange={handleFileSelect} />
-          </div>
           <div className="col-lg-6 mt-2">
             <label htmlFor="note">Note:<Mandatory/></label>
             <textarea rows="4" className="form-control" id="note" value={note} onChange={e => setNote(e.target.value)}/>
-          </div>
-          <div className="col-lg-6 mt-2">
-
           </div>
           <div className="col-lg-12 mt-2">
             <button type="submit" className="btn btn-sm btn-primary me-2">Submit</button>
--- src/components/ViewNote.js
+++ src/components/ViewNote.js
@@ -6,30 +6,10 @@ import useViewHeader from './hooks/useViewHeader.js'
 const ViewNote = ({secret, secretAction}) => {
   const [expanded, toggle, edit, del] = useViewHeader(secret, secretAction)
 
-  const hasFile = secret.file && secret.file.name != ''
-  const isImage = hasFile && secret.file.content.startsWith('data:image')
-
   return (
     <ViewTable alertType="alert-warning" secret={secret} expanded={expanded} toggle={toggle} edit={edit} del={del}>
       <tr><td><b>Type:</b></td><td><b>{secret.type}</b></td></tr>
       <tr><td><b>Note:</b></td><td><pre style={{whiteSpace: 'pre-wrap'}}>{secret.note}</pre></td></tr>
-      {hasFile && (
-        <tr>
-          <td><b>File:</b></td>
-          <td style={{wordWrap: 'anywhere'}}>
-            <p>
-              <a href={secret.file.content} title='Click to download' target="_self"
-              download={secret.file.name}>
-                {secret.file.name}
-              </a>
-              &nbsp;({Utils.numberFormat(secret.file.size)} bytes)
-            </p>
-            {isImage && (
-              <img src={secret.file.content} className="rounded" style={{width: '100%'}}/>
-            )}
-          </td>
-        </tr>
-      )}
     </ViewTable>
   )
 }
